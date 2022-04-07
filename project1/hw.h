686a09726ad0b01636aed60d1387ab71028097cf
duyeun330 <dooyeun330@u.sogang.ac.kr>
2022-04-07 21:07:43 +0900

    editted in virtualbox

--- project1/hw.h
+++ project1/hw.h
@@ -5,11 +5,22 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <fcntl.h>
+#include <string.h>
+#include <dirent.h>
+#include <linux/input.h>
+#include <termios.h>
+#include <signal.h>
+#include <string.h>
 
 // MODE
 #define CLOCK 1
@@ -20,7 +31,7 @@
 // SHARED MEMORY & SEMAPHORE KEY-GEN
 #define SHARED_KEY1 (key_t) 0x10
 #define SHARED_KEY2 (key_t) 0x20
-#define SEM_KEY (key_t) 0x20
+#define SEM_KEY (key_t) 0x30
 #define ERR ((struct databuf *)-1)
 
 // STRUCT SHARED MEMORY
@@ -30,6 +41,8 @@ struct databuf1{
 	int	mode;
 	int	switch_num;
 	char	d_buf[1001];
+	unsigned char	sw_buf[10];
+	unsigned char	fnd_buf[4];
 };
 
 struct databuf1 *input_buffer, *output_buffer;
@@ -41,4 +54,12 @@ int shm_id1, shm_id2, sem_id;
 struct sembuf p1 = {0, -1, SEM_UNDO }, p2 = {1, -1, SEM_UNDO};
 struct sembuf v1 = {0, 1, SEM_UNDO }, v2 = {1, 1, SEM_UNDO};
 
+// DEVICE DRIVER FD
+int	dev_fd[10];
+
+// For Mode 1
+int	is_edit;
+struct tm	*loc_time;
+time_t	t;
+unsigned char	sw_buf[10];
 #endif
